4dd67f4c4e8edb2bab522b79cb209f1ebcfe0188
Mboulgour <mathieu.r.77@gmail.com>
2021-05-20 13:51:59 +0200

    New release update

--- app/src/main/java/com/logora/testapp/MainActivity.java
+++ app/src/main/java/com/logora/testapp/MainActivity.java
@@ -13,12 +13,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String applicationName = "logora-demo";
-        String authAssertion = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZmlyc3RfbmFtZSI6IlZpdGFsaWsiLCJsYXN0X25hbWUiOiJCdXRlcmluIiwiZW1haWwiOiJ2aXRhbGlrQGJ1dGVyaW4uY29tIiwidWlkIjoiMTIzNDI0IiwiaWF0IjoxNTE2MjM5MDIyfQ.KEQ6fHyFHu34-dGLYGnbBR_LF6BdOWnz2P9GyYZPJBg---";
-        WidgetFragment widget = new WidgetFragment(this.getApplicationContext(), "mon-uid", applicationName, authAssertion);
+        WidgetFragment widget = new WidgetFragment(this.getApplicationContext(), "1162254885", applicationName);
 
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.logora_widget_container, widget)
                 .commit();
-
     }
 }
