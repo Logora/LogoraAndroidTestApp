ea99d4e919976048453c7baa3a455d54c9fb5a56
somayaASS <somaya.assaadi@gmail.com>
2023-09-27 11:57:00 +0200

    updated build-gradle & AndroidManifest

--- app/src/main/java/com/logora/testapp/MainActivity.java
+++ app/src/main/java/com/logora/testapp/MainActivity.java
@@ -1,19 +1,15 @@
 package com.logora.testapp;
 
-import android.app.Activity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 import com.logora.logora_sdk.WidgetFragment;
-
-    public class MainActivity extends Activity {
-
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String applicationName = "logora-demo";
-        WidgetFragment widget = new WidgetFragment(this.getApplicationContext(), "1162254885", applicationName);
-
+        WidgetFragment widget = new WidgetFragment(this, "mon-article", "logora-demo-app");
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.logora_widget_container, widget)
                 .commit();
