683dd2607eed8462aae5eec341d9aee5d627ff09
somayaASS <somaya.assaadi@gmail.com>
2023-10-06 11:21:19 +0200

    Ignore ./idea in .gitignore, add Jwt, and  update Github token

--- app/src/main/java/com/logora/testapp/MainActivity.java
+++ app/src/main/java/com/logora/testapp/MainActivity.java
@@ -3,13 +3,14 @@ package com.logora.testapp;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import com.logora.logora_sdk.WidgetFragment;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        WidgetFragment widget = new WidgetFragment(this, "mon-article", "logora-demo-app");
+        String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiIxMjM0NTY3ODI5IiwiZmlyc3RfbmFtZSI6IlNveWEiLCJsYXN0X25hbWUiOiJTb3lhIiwiZW1haWwiOiJzb3lhQGdtYWlsLmNvbSJ9.s213UFjD7kXAsQ2-foJvJ1S3Im6oKuUxgEWTYD4kn9Y";
+        WidgetFragment widget = new WidgetFragment(this, "mon-article", "logora-demo-app",null);
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.logora_widget_container, widget)
                 .commit();
